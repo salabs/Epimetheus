b607372daf3d5d9b3730dff5e4f92ba90abb2772
Ville Friman <ville.friman@siili.com>
2020-05-13 12:47:45 +0300

    Removes locales/encommon.js file
    Translations in that file were not used anywhere
    so it can be removed.

--- frontend/src/utils/i118n.js
+++ frontend/src/utils/i118n.js
@@ -1,12 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import frontpage from '../locales/en/frontpage.json';
-import common from '../locales/en/common.json';
 import mainnav from '../locales/en/mainnav.json';
 
 const resources = {
   en: {
-    common,
     frontpage,
     mainnav
   }
