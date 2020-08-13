e7833d432621d167800854557c4ed23d3cb373dc
Oliver Rikkilä <oliver.rikkila@siili.com>
2020-08-13 12:28:35 +0300

    codacy tests

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,5 +1,5 @@
-﻿﻿import React from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+﻿﻿import { NavLink, useLocation } from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { useStateValue } from '../../contexts/state';
