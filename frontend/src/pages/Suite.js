561d3c2bbf9e2a63ea143083f2206d1671ce766e
Oliver Rikkilä <oliver.rikkila@siili.com>
2020-08-24 12:36:45 +0300

    unused code

--- frontend/src/pages/Suite.js
+++ frontend/src/pages/Suite.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import React, { useState } from 'react';
+import React from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useStateValue } from '../contexts/state';
