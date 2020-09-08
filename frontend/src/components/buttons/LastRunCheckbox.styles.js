10b5e5f41bc47bdca92cd62db99af18e11d4675d
Oliver Rikkilä <oliver.rikkila@siili.com>
2020-09-08 16:01:11 +0300

    robot text fix attempt

--- frontend/src/components/buttons/LastRunCheckbox.styles.js
+++ frontend/src/components/buttons/LastRunCheckbox.styles.js
@@ -38,13 +38,5 @@ export const StyledLabel = styled.label`
 `;
 
 export const StyledInput = styled.input`
-    border: 0;
-    clip: rect(0 0 0 0);
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-    white-space: nowrap;
-    width: 1px;
+    display: none;
 `;
