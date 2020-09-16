71c1e20def320ea17ac103fa5067e3481fa249e0
Oliver Rikkilä <oliver.rikkila@siili.com>
2020-09-16 12:53:21 +0300

    breadcrumb coloring properties

--- frontend/src/components/BreadcrumbNav.styles.js
+++ frontend/src/components/BreadcrumbNav.styles.js
@@ -4,6 +4,20 @@ import { Link } from 'react-router-dom';
 export const BreadcrumbContainer = styled.div`
     font-size: 14px;
     margin: 10px 0px;
+
+    a {
+        text-decoration: none;
+    }
+
+    .SeriesBreadCrumb {
+        color: ${props => props.status === 'series' && 'var(--pirlo-blue)'};
+        font-weight: ${props => props.status === 'series' && 'bolder'};
+    }
+
+    .BuildBreadCrumb {
+        color: ${props => props.status === 'build' && 'var(--pirlo-blue)'};
+        font-weight: ${props => props.status === 'build' && 'bolder'};
+    }
 `;
 
 export const StyledInnerDiv = styled.div`
