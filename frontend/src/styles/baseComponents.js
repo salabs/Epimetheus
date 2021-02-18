0cc7616b95f505958ddfcef4bd10e757c7c2e793
Tiia Valtonen <tiia.valtonen@siili.com>
2021-02-18 13:05:39 +0200

    Create container and content grids

--- frontend/src/styles/baseComponents.js
+++ frontend/src/styles/baseComponents.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const baseTable = styled.table`
+export const baseTable = styled.table`
     table-layout: fixed;
     width: 100%;
     border-collapse: separate !important;
@@ -39,7 +39,7 @@ const baseTable = styled.table`
     }
 `;
 
-const overviewElement = styled.div`
+export const overviewElement = styled.div`
     padding: 10px;
     border-style: solid;
     border-width: thin;
@@ -48,4 +48,14 @@ const overviewElement = styled.div`
     height: min-content;
 `;
 
-export { baseTable, overviewElement };
+export const ContainerGrid12 = styled.div`
+    max-width: calc(var(--max-page-width) + calc(100% / 12) + calc(100% / 12));
+    padding-right: calc(100% / 12);
+    padding-left: calc(100% / 12);
+    margin: 0 auto;
+`;
+
+export const ContentGrid6 = styled.div`
+    max-width: 100%;
+    margin: 0 calc(100% / 12);
+`;
