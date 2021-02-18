684ca22cc62785be94b599e9e2ab223602203b0d
Tiia Valtonen <tiia.valtonen@siili.com>
2021-02-18 13:08:02 +0200

    Refactor 'Frontpage' to use grid

--- frontend/src/pages/Frontpage.styles.js
+++ frontend/src/pages/Frontpage.styles.js
@@ -1,15 +1,12 @@
 ﻿import styled from 'styled-components';
 
-export const FrontPage = styled.div`
-    width: 100%;
-`;
-
 export const IconsContainer = styled.div`
     display: flex;
     justify-content: space-around;
-    padding-top: 20px;
+    flex-wrap: wrap;
 
     div {
+        padding-top: 20px;
         min-height: 100px;
         max-width: 150px;
     }
