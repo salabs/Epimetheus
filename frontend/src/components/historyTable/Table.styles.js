3452fc408d6343244d97dadc97411833e4797c7d
Tiia Valtonen <tiia.valtonen@siili.com>
2021-02-19 12:43:54 +0200

    Fix series's history page's table and add a little indicator that table is scrollable

--- frontend/src/components/historyTable/Table.styles.js
+++ frontend/src/components/historyTable/Table.styles.js
@@ -1,7 +1,49 @@
 ﻿import styled from 'styled-components';
 
+export const TableWrapper = styled.div`
+    max-width: calc(100% + 40px);
+    position: relative;
+    margin: 0 -20px;
+    &::before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '';
+        width: 20px;
+        height: 100%;
+        background-image: linear-gradient(
+            to right,
+            white,
+            rgba(255, 255, 255, 0.001)
+        );
+        z-index: 1;
+    }
+
+    &::after {
+        position: absolute;
+        top: 0;
+        right: 0;
+        content: '';
+        width: 20px;
+        height: 100%;
+        background-image: linear-gradient(
+            to left,
+            white,
+            rgba(255, 255, 255, 0.001)
+        );
+    }
+`;
+
+export const OverflowWrapper = styled.div`
+    display: inline-block;
+    overflow-x: auto;
+    position: relative;
+    max-width: 100%;
+    width: 100%;
+    padding: 0 20px;
+`;
+
 export const TableStyled = styled.table`
-    overflow: auto;
     clear: both;
     border-collapse: separate !important;
     border-spacing: 0;
@@ -9,7 +51,18 @@ export const TableStyled = styled.table`
     border-radius: 8px;
     text-align: left;
     vertical-align: top;
+    position: relative;
     width: 100%;
+
+    &::after {
+        content: '';
+        position: absolute;
+        right: -20px;
+        top: 0;
+        display: inline-block;
+        width: 20px;
+        height: 100%;
+    }
 
     thead {
         background: var(--hermanni-grey);
