f9b17cfb6dbbb9df18374e92891b7b0e5f639db7
Tiia Valtonen <tiia.valtonen@siili.com>
2021-03-11 07:48:51 +0200

    143 - Create scroll buttons for table that are enabled when table is scrollable (takes into account window resize and scrolling without using the scroll buttons)

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     OverflowWrapper,
     SimpleTable,
@@ -7,27 +7,101 @@ import {
 } from './Table.styles';
 import ScrollTableButton from './ScrollTableButton';
 
-export const TableSpreadSheet = props => {
+export const Table = props => {
     const tableId = props['table-id'];
+    const simpleTable = props['simple-table'];
+    const ref = React.createRef();
+
+    const [canScrollLeft, setCanScrollLeft] = useState(false);
+    const [canScrollRight, setCanScrollRight] = useState(false);
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [windowSize, setWindowSize] = useState([0, 0]);
+    const maxScrollLeft = React.useRef(0);
+
+    // Checks if table is scrolled using other methods than scrollButtons (e.g. keyboard / mouse) ==> scrollPosition
+    useEffect(() => {
+        const savedRef = ref.current;
+
+        function updateScrollPosition() {
+            setScrollPosition(savedRef.scrollLeft);
+        }
+
+        savedRef.addEventListener('scroll', updateScrollPosition);
+        updateScrollPosition();
+
+        return () =>
+            savedRef.removeEventListener('scroll', updateScrollPosition);
+    }, [ref]);
+
+    // Checks if window is resized ==> windowSize
+    useEffect(() => {
+        function updateWindowSize() {
+            setWindowSize([window.innerWidth, window.innerHeight]);
+        }
+
+        window.addEventListener('resize', updateWindowSize);
+        updateWindowSize();
+
+        return () => window.removeEventListener('resize', updateWindowSize);
+    }, []);
+
+    // Checks if table is scrollable and decides to which directions it's scrollable
+    useEffect(() => {
+        maxScrollLeft.current =
+            ref.current.scrollWidth - ref.current.clientWidth;
+
+        // check that table is scrollable and it has not been scrolled
+        if (maxScrollLeft.current !== 0 && scrollPosition === 0) {
+            setCanScrollRight(true);
+            setCanScrollLeft(false);
+        }
+
+        // show scroll left and right button
+        if (scrollPosition !== 0) {
+            setCanScrollLeft(true);
+            setCanScrollRight(true);
+        }
+
+        // hide scroll left button when there's nothing to scroll
+        if (scrollPosition === 0) {
+            setCanScrollLeft(false);
+        }
+
+        // hide scroll right button when there's nothing to scroll
+        if (
+            scrollPosition === maxScrollLeft.current ||
+            scrollPosition === maxScrollLeft.current - 1
+        ) {
+            setCanScrollRight(false);
+        }
+    }, [ref, scrollPosition, windowSize]);
+
+    function moveRight() {
+        ref.current.scrollLeft += 150;
+        setScrollPosition(scrollPosition + 150);
+    }
+
+    function moveLeft() {
+        ref.current.scrollLeft -= 150;
+        setScrollPosition(scrollPosition - 150);
+    }
 
     return (
         <TableWrapper>
-            <ScrollTableButton table-id={tableId} />
-            <OverflowWrapper id={tableId}>
-                <SpreadSheetTable>{props.children}</SpreadSheetTable>
-            </OverflowWrapper>
-        </TableWrapper>
-    );
-};
-
-export const TableSimple = props => {
-    const tableId = props['table-id'];
-
-    return (
-        <TableWrapper>
-            <ScrollTableButton table-id={tableId} />
-            <OverflowWrapper id={tableId}>
-                <SimpleTable>{props.children}</SimpleTable>
+            <ScrollTableButton
+                canScrollLeft={canScrollLeft}
+                canScrollRight={canScrollRight}
+                moveRight={() => moveRight()}
+                moveLeft={() => moveLeft()}
+            />
+            <OverflowWrapper ref={ref}>
+                {simpleTable ? (
+                    <SimpleTable id={tableId}>{props.children}</SimpleTable>
+                ) : (
+                    <SpreadSheetTable id={tableId}>
+                        {props.children}
+                    </SpreadSheetTable>
+                )}
             </OverflowWrapper>
         </TableWrapper>
     );
