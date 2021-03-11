e2405efa361dd2036f2f8c229dfee7bfe077c97d
Tiia Valtonen <tiia.valtonen@siili.com>
2021-03-11 11:06:01 +0200

    143 - Refactor All Build view's table to use the new table component and rename it

---
+++ frontend/src/components/historyTable/HistoryTable.jsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line
+import React from 'react';
+import Heading from './Heading';
+import Body from './Body';
+import NotFound from '../NotFound';
+import { useStateValue } from '../../contexts/state';
+import { Table } from '../table/Table';
+
+const HistoryTable = () => {
+    const [
+        {
+            historyDataState: { max_build_num },
+        },
+    ] = useStateValue();
+
+    if (max_build_num > 0) {
+        return (
+            <Table table-id="history-table">
+                <Heading />
+                <tbody id="history-table-body">
+                    <Body />
+                </tbody>
+            </Table>
+        );
+    } else {
+        return <NotFound />;
+    }
+};
+
+export default HistoryTable;
