e3a47ab582773baae68cbdef5dcaa597b19acd3b
Tiia Valtonen <tiia.valtonen@siili.com>
2021-03-11 07:48:51 +0200

    143 - Create scroll buttons for table that are enabled when table is scrollable (takes into account window resize and scrolling without using the scroll buttons)

---
+++ frontend/src/components/table/ScrollTableButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ScrollButton } from './ScrollTableButton.styles';
+import ChevronRight from '../../images/chevron-right.svg';
+import ChevronLeft from '../../images/chevron-left.svg';
+
+const ScrollTableButton = props => {
+    const { canScrollLeft, canScrollRight } = props;
+
+    return (
+        <>
+            <ScrollButton
+                className="left"
+                onClick={() => props.moveLeft()}
+                disabled={!canScrollLeft}
+            >
+                <img src={ChevronLeft} alt=">" />
+            </ScrollButton>
+
+            <ScrollButton
+                className="right"
+                onClick={() => props.moveRight()}
+                disabled={!canScrollRight}
+            >
+                <img src={ChevronRight} alt=">" />
+            </ScrollButton>
+        </>
+    );
+};
+
+export default ScrollTableButton;
