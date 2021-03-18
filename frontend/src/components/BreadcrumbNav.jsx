a75652ae1e7e282710e0783cc438d785c029b188
Tiia Valtonen <tiia.valtonen@siili.com>
2021-03-18 08:31:23 +0200

    155 - Refactor Breadcrumb a bit so there's no need to repeat link styles

--- frontend/src/components/BreadcrumbNav.jsx
+++ frontend/src/components/BreadcrumbNav.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useStateValue } from '../contexts/state';
 import { useParams } from 'react-router';
-import {
-    BreadcrumbContainer,
-    StyledInnerDiv,
-    StyledLink,
-    TeamsLink,
-} from './BreadcrumbNav.styles';
+import { BreadcrumbContainer, StyledInnerOl } from './BreadcrumbNav.styles';
 import { ContainerGrid12, ContentGrid6 } from '../styles/baseComponents';
+import { Link } from 'react-router-dom';
 
 const BreadcrumbTeams = () => {
     return (
         <li>
-            <TeamsLink to={`/team`}>Teams</TeamsLink>
+            <Link to={`/team`}>Teams</Link>
         </li>
     );
 };
@@ -22,20 +18,20 @@ const BreadcrumbItemTeam = props => {
     const [{ selectedBranchState }] = useStateValue();
     const teamName = name || selectedBranchState.team;
     return (
-        <React.Fragment>
+        <>
             <BreadcrumbTeams />
             <li>
-                <span>/</span>
-                <StyledLink
-                    aria-current={props.current ? 'page' : false}
+                <span aria-hidden="true">/</span>
+                <Link
                     to={`/team/${teamName}`}
-                    className="TeamBreadCrumb"
                     id="TeamBreadCrumb"
+                    className={props.current ? 'active' : ''}
+                    aria-current={props.current ? 'page' : false}
                 >
                     {teamName}
-                </StyledLink>
+                </Link>
             </li>
-        </React.Fragment>
+        </>
     );
 };
 
@@ -44,63 +40,63 @@ const BreadcrumbItemSeries = props => {
     const [{ selectedBranchState }] = useStateValue();
     const seriesId = series || selectedBranchState.id;
     return (
-        <React.Fragment>
+        <>
             <BreadcrumbItemTeam />
             <li>
-                <span>/</span>
-                <StyledLink
-                    aria-current={props.current ? 'page' : false}
+                <span aria-hidden="true">/</span>
+                <Link
                     to={`/series/${seriesId}/overview`}
                     id="SeriesBreadCrumb"
-                    className="SeriesBreadCrumb"
+                    className={props.current ? 'active' : ''}
                     aria-label={'series: ' + selectedBranchState.name}
+                    aria-current={props.current ? 'page' : false}
                 >
                     {selectedBranchState.name}
-                </StyledLink>
+                </Link>
             </li>
-        </React.Fragment>
+        </>
     );
 };
 
 const BreadcrumbItemBuild = props => {
     const { buildId, seriesId } = useParams();
     return (
-        <React.Fragment>
+        <>
             <BreadcrumbItemSeries />
             <li>
-                <span>/</span>
-                <StyledLink
-                    aria-current={props.current ? 'page' : false}
+                <span aria-hidden="true">/</span>
+                <Link
                     to={`/series/${seriesId}/build/${buildId}/overview`}
                     id="BuildBreadCrumb"
-                    className="BuildBreadCrumb"
+                    className={props.current ? 'active' : ''}
                     aria-label={`buildId ${buildId}`}
+                    aria-current={props.current ? 'page' : false}
                 >
                     {buildId}
-                </StyledLink>
+                </Link>
             </li>
-        </React.Fragment>
+        </>
     );
 };
 
 const BreadcrumbItemSuite = props => {
     const { seriesId, buildId, suiteId } = useParams();
     return (
-        <React.Fragment>
+        <>
             <BreadcrumbItemBuild />
             <li>
-                <span>/</span>
-                <StyledLink
-                    aria-current={props.current ? 'page' : false}
+                <span aria-hidden="true">/</span>
+                <Link
                     to={`/series/${seriesId}/build/${buildId}/suite/${suiteId}/history`}
                     id="SuiteBreadCrumb"
-                    className="SuiteBreadCrumb"
-                    aria-label={`SuiteId ${suiteId}`}
+                    className={props.current ? 'active' : ''}
+                    aria-label={`suiteId ${suiteId}`}
+                    aria-current={props.current ? 'page' : false}
                 >
                     {suiteId}
-                </StyledLink>
+                </Link>
             </li>
-        </React.Fragment>
+        </>
     );
 };
 
@@ -113,17 +109,19 @@ const BREADCRUMB_STATUS = {
 
 const BreadcrumbNav = ({ status }) => {
     return (
-        <BreadcrumbContainer id={'breadCrumbNav'} status={status}>
-            <ContainerGrid12>
-                <ContentGrid6>
-                    <nav aria-label="Breadcrumb">
-                        <StyledInnerDiv>
-                            {BREADCRUMB_STATUS[`${status}`]}
-                        </StyledInnerDiv>
-                    </nav>
-                </ContentGrid6>
-            </ContainerGrid12>
-        </BreadcrumbContainer>
+        <ContainerGrid12>
+            <ContentGrid6>
+                <BreadcrumbContainer
+                    id={'breadCrumbNav'}
+                    status={status}
+                    aria-label="Breadcrumb"
+                >
+                    <StyledInnerOl>
+                        {BREADCRUMB_STATUS[`${status}`]}
+                    </StyledInnerOl>
+                </BreadcrumbContainer>
+            </ContentGrid6>
+        </ContainerGrid12>
     );
 };
 
