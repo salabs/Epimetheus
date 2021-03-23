1f5591e845b3c7dc465926aec340ed2ee0da380f
Tiia Valtonen <tiia.valtonen@siili.com>
2021-03-23 13:19:53 +0200

    159 - Create rounded corners for first and last row's first and last SeriesCard

--- frontend/src/components/card/SeriesCard.styles.js
+++ frontend/src/components/card/SeriesCard.styles.js
@@ -4,13 +4,84 @@ import { Link } from 'react-router-dom';
 export const CardSection = styled.section`
     background-color: var(--nero-white);
     box-shadow: 0 2px var(--space-4) var(--hermanni-grey);
-    border-radius: var(--space-8) 0 0 0;
     border: 1px solid var(--hermanni-grey);
     display: grid;
     padding: 0 var(--space-16);
     min-height: 232px;
     word-break: break-word;
+
+    ${roundCorners()};
 `;
+
+function roundCorners() {
+    let rules = [];
+    for (let i = 0; i < 4; i++) {
+        const pageMinWidth = 363;
+        const breakpointWidth = 385;
+        const mediaQuery = `@media only screen and
+                    (min-width: ${pageMinWidth + breakpointWidth * i}px) and
+                    (max-width: ${pageMinWidth + breakpointWidth * (i + 1)}px)`;
+        const everyNth = `${i + 1}n + 1`;
+
+        rules.push(`${mediaQuery} {
+            // only card OR first row's last card
+            &:only-of-type,
+            &:nth-of-type(${i + 1}) {
+                border-top-right-radius: var(--space-8);
+            }
+
+            // first card
+            &:first-of-type {
+                border-top-left-radius: var(--space-8);
+
+                // next card to first card is also the last
+                &+:last-of-type {
+                    border-top-right-radius: var(--space-8);
+                }
+            }
+
+            // last row's first and last card OR last full row's first card
+            &:nth-of-type(${everyNth}):last-of-type,
+            &:nth-of-type(${everyNth}):nth-last-of-type(${i + 1}) {
+                border-bottom-left-radius: var(--space-8);
+            }
+
+            // last card
+            &:last-of-type {
+                border-bottom-right-radius: var(--space-8);
+
+                // last card that's also second last card (first out of two)
+                &:nth-last-of-type(2) {
+                    border-top-right-radius: var(--space-8);
+                }
+            }
+        }`);
+        if (i >= 2) {
+            // when grid row can contain more than 3 cards, but last row only contains:
+            // 2-3 cards in 4 card row OR
+            // 2 cards in 3 card row
+            rules.push(
+                `${mediaQuery} {
+                        &:nth-of-type(${everyNth}):nth-last-of-type(2),
+                        &:nth-of-type(${everyNth}):nth-last-of-type(${i}) {
+                            border-bottom-left-radius: var(--space-8);
+                        }
+                    }`
+            );
+        }
+        if (i === 3) {
+            // if grid row can contain 4 cards, but there's only 3
+            rules.push(
+                `${mediaQuery} {
+                        &:nth-of-type(${i}):last-of-type {
+                            border-top-right-radius: var(--space-8);
+                        }
+                    }`
+            );
+        }
+    }
+    return rules;
+}
 
 export const CardHeading = styled.span`
     font-size: 20px;
