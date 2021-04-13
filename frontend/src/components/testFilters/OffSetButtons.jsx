c8aa20c8213acc050d1a20661e5d7a519a0d67d4
Oliver <turso@olen.to>
2021-04-13 14:03:35 +0300

    Bug/177 offset reset (#179)
    * 177 - Offset does not reset when changing to another Team

--- frontend/src/components/testFilters/OffSetButtons.jsx
+++ frontend/src/components/testFilters/OffSetButtons.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-// eslint-disable-next-line
 import React, { useState, useEffect } from 'react';
 import { useStateValue } from '../../contexts/state';
 import { useHistory, useLocation } from 'react-router-dom';
@@ -76,6 +75,12 @@ const Offset = () => {
             setrightDisabled(false);
         }
     }, [offset, inputOffset, builds]);
+
+    useEffect(() => {
+        return () => {
+            dispatch({ type: 'flushQueryParams' });
+        };
+    }, []);
 
     const handleLatestButtonPress = () => {
         dispatch({
